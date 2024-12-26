46507cfa778b9082745eadec510a92e9f279aacd
Mukund Kansara <mukundkansara2411@gmail.com>
2024-12-26 10:43:07 +0530

    Add Recursive Approach for Reverse Linked List

--- Reverse_Linked_List.cpp
+++ Reverse_Linked_List.cpp
@@ -1,9 +1,10 @@
-LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+// Iterative Approach
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
 {
-    LinkedListNode<int>* prev = NULL;
-    LinkedListNode<int>* curr = head;
-    LinkedListNode<int>* forward = NULL;
-    while(curr!= NULL)
+    LinkedListNode<int> *prev = NULL;
+    LinkedListNode<int> *curr = head;
+    LinkedListNode<int> *forward = NULL;
+    while (curr != NULL)
     {
         forward = curr->next;
         curr->next = prev;
@@ -11,4 +12,24 @@ LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
         curr = forward;
     }
     return prev;
+}
+
+// Recursive Approach
+void reverse(LinkedListNode<int> *&head, LinkedListNode<int> *curr, LinkedListNode<int> *prev)
+{
+    if (curr == NULL)
+    {
+        head = prev;
+        return;
+    }
+    LinkedListNode<int> *forward = curr->next;
+    reverse(head, forward, curr);
+    curr->next = prev;
+}
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
+{
+    LinkedListNode<int> *prev = NULL;
+    LinkedListNode<int> *curr = head;
+    reverse(head, curr, prev);
+    return head;
 }
