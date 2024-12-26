49fc2c0bdf753559eddbc181fe42f6e4b0f10187
Mukund Kansara <mukundkansara2411@gmail.com>
2024-12-26 09:52:36 +0530

    Added Reverse Linked List

---
+++ Reverse_Linked_List.cpp
@@ -0,0 +1,14 @@
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int>* prev = NULL;
+    LinkedListNode<int>* curr = head;
+    LinkedListNode<int>* forward = NULL;
+    while(curr!= NULL)
+    {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
