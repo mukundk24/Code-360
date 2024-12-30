8b5e293c79e28f0e25177aaed1b5a0fc7f8475ca
Mukund Kansara <mukundkansara2411@gmail.com>
2024-12-30 16:59:28 +0530

    Add Another Approach for Sort linked list of 0s 1s 2s

--- Sort_linked_list_of_0s_1s_2s.cpp
+++ Sort_linked_list_of_0s_1s_2s.cpp
@@ -33,3 +33,42 @@ Node *sortList(Node *head)
     }
     return head;
 }
+
+// Another Approach without replacing data
+void Insert(Node *&tail, Node *curr)
+{
+    tail->next = curr;
+    tail = tail->next;
+}
+Node *sortList(Node *head)
+{
+    Node *head0 = new Node(-1);
+    Node *tail0 = head0;
+    Node *head1 = new Node(-1);
+    Node *tail1 = head1;
+    Node *head2 = new Node(-1);
+    Node *tail2 = head2;
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if (curr->data == 0)
+            Insert(tail0, curr);
+        else if (curr->data == 1)
+            Insert(tail1, curr);
+        else
+            Insert(tail2, curr);
+        curr = curr->next;
+    }
+    if (head1->next != NULL)
+        tail0->next = head1->next;
+    else
+        tail0->next = head2->next;
+    tail1->next = head2->next;
+    tail2->next = NULL;
+    head = head0->next;
+    delete head0;
+    delete head1;
+    delete head2;
+    return head;
+}
