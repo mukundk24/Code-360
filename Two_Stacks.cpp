e5480a6f92b2477e78a1935ce623b8c66073dda3
Mukund Kansara <mukundkansara2411@gmail.com>
2025-01-07 16:04:56 +0530

    Create Solution of Two Stacks

---
+++ Two_Stacks.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+class TwoStack {
+
+public:
+  // Initialize TwoStack.
+  int *arr;
+  int top1, top2;
+  int size;
+  TwoStack(int s) {
+    top1 = -1;
+    top2 = s;
+    size = s;
+    arr = new int[s];
+  }
+
+  // Push in stack 1.
+  void push1(int num) {
+    if (top2 - top1 > 1) {
+      top1++;
+      arr[top1] = num;
+    }
+  }
+
+  // Push in stack 2.
+  void push2(int num) {
+    if (top2 - top1 > 1) {
+      top2--;
+      arr[top2] = num;
+    }
+  }
+
+  // Pop from stack 1 and return popped element.
+  int pop1() {
+    if (top1 >= 0) {
+      int val = arr[top1];
+      top1--;
+      return val;
+    } else {
+      return -1;
+    }
+  }
+
+  // Pop from stack 2 and return popped element.
+  int pop2() {
+    if (top2 < size) {
+      int val = arr[top2];
+      top2++;
+      return val;
+    } else {
+      return -1;
+    }
+  }
+};
